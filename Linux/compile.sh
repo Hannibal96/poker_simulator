69cec232edd69058ff815336dcb237ca91ff9514
Hannibal96 <neriauzan@gmail.com>
2024-01-26 17:55:36 +0200

    Linux

---
+++ Linux/compile.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+g++ ./../main.cpp ./../agents_utils/*.cpp ./../agents_utils/*.h ./../poker_utils/*.cpp ./../poker_utils/*.h ./../deck_utils/*.cpp ./../deck_utils/*.h ./../jsons_utils/*.cpp ./../jsons_utils/*.h -o simulator.exe
+
+
