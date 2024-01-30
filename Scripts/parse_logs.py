5a17b1cdf0b1f61281e97b8e4e5a03fb504cfb72
neria uzan <neriauzan@gmail.com>
2024-01-30 22:40:09 +0200

    logs

--- Scripts/parse_logs.py
+++ Scripts/parse_logs.py
@@ -1,101 +1,207 @@
-import glob
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 
-CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-STATE = ["CO", "DE", "DE_CO", "SB", "SB_CO", "SB_DE", "SB_CO_DE", "BB_CO", "BB_DE", "BB_SB", "BB_CO_DE", "BB_CO_SB", "BB_DE_SB", "BB_CO_DE_SB"]
-TAB_SPACE = 8
+card_val_d = {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'T': 10,
+    'J': 11,
+    'Q': 12,
+    'K': 13,
+    'A': 14,
+}
 
 
-def extract_tables(logger):
-    last_line = None
-    with open(log, 'r') as f:
-        for idx, line in enumerate(f):
-            if "============================******************************************************============================" in line:
-                last_line = idx
+situation_d = {
+    "CO": 0,
+    "DE": 1,
+    "DE_CO": 2,
+    "SB": 3,
+    "SB_CO": 4,
+    "SB_DE": 5,
+    "SB_CO_DE": 6,
+    "BB_CO": 7,
+    "BB_DE": 8,
+    "BB_SB": 9,
+    "BB_CO_DE": 10,
+    "BB_CO_SB": 11,
+    "BB_DE_SB": 12,
+    "BB_CO_DE_SB": 13,
+}
 
-    with open(log, 'r') as f:
-        content = f.read()
-    res = content.split("\n")[last_line:]
-
-    tabels_line = []
-    for idx, line in enumerate(res):
-        if "22o" in line:
-            tabels_line.append(idx)
-
-    co_idx, de_idx, sb_idx, bb_idx = tabels_line
-    co_table = "\n".join(res[co_idx:co_idx + 169])
-    de_table = "\n".join(res[de_idx:de_idx + 169])
-    sb_table = "\n".join(res[sb_idx:sb_idx + 169])
-    bb_table = "\n".join(res[bb_idx:bb_idx + 169])
-
-    return co_table, de_table, sb_table, bb_table
-
-
-def create_res_dict():
-    res_dict = {}
-    for idx_a, card_a in enumerate(CARDS):
-        for idx_b, card_b in enumerate(CARDS[0:idx_a+1]):
-            for suit in ['o', 's']:
-                if suit == 's' and card_a == card_b:
-                    continue
-                hand = card_a+card_b+suit
-                for state in STATE:
-                    res_dict[(hand, state)] = 0
-    return res_dict
+player_d = {
+    "CutOff": 0,
+    "Dealer": 1,
+    "SmallBlind": 2,
+    "BigBlind": 3,
+}
 
 
-def parse_table(table, indices, states, res_dict):
-    for line in table.split("\n"):
-        entries = line.split("\t")
-        hand = entries[0]
-        for i, s in zip(indices, states):
-            if "A" in entries[i]:
-                res_dict[(hand, s)] += 1
-            elif "F" in entries[i]:
+def hand_str_2_idx(hand):
+    M = hand[0]
+    m = hand[1]
+    suit = hand[2]
+    M = card_val_d[M] - 2
+    m = card_val_d[m] - 2
+    suit = int(suit == 's')
+    return M ** 2 + 2 * m + suit
+
+
+def count_lines(filename):
+    with open(filename, 'r') as file:
+        return sum(1 for _ in file)
+
+
+def parse_mwu_log(file):
+    N = round(count_lines(file) / 744)
+    file = open(file)
+    data_results = np.zeros([4, N, 14, 169])
+    curr_t = -1
+    curr_player = -1
+    for i, line in enumerate(file):
+        if "Position: CutOff" in line:
+            curr_player = 0
+            curr_t += 1
+        if "Position: Dealer" in line:
+            curr_player = 1
+        if "Position: SmallBlind" in line:
+            curr_player = 2
+        if "Position: BigBlind" in line:
+            curr_player = 3
+
+        split_line = line.split("|")
+        if len(split_line) == 16:
+            if split_line[0][0] == " ":
                 continue
             else:
-                assert False
-    return res_dict
+                hand_idx = hand_str_2_idx(hand=split_line[0])
+                values = [float(i) for i in split_line[1:-1]]
+                data_results[curr_player, curr_t, :, hand_idx] = values
+
+    return data_results
 
 
-def print_dict(res_dict, n):
-    print("\t", end="")
-    for idx, state in enumerate(STATE):
-        end = "\t" * (2 - len(state) // TAB_SPACE)
-        print(state, end=end)
-    print(end="\n")
-    for idx_a, card_a in enumerate(CARDS):
-        for idx_b, card_b in enumerate(CARDS[0:idx_a+1]):
-            for suit in ['o', 's']:
-                if suit == 's' and card_a == card_b:
-                    continue
-                hand = card_a+card_b+suit
-                print(hand, end="\t")
-                for state in STATE:
-                    val = round(100 * res_dict[(hand, state)]/n, 3)
-                    if len(str(val)) >= TAB_SPACE:
-                        end = "\t"
+def parse_bandit_log(file):
+    N = round(count_lines(file) / 744)
+    file = open(file)
+    data_results = np.zeros([4, N, 14, 169])
+    curr_t = -1
+    curr_player = -1
+    for i, line in enumerate(file):
+        if "Position: CutOff" in line:
+            curr_player = 0
+            curr_t += 1
+        if "Position: Dealer" in line:
+            curr_player = 1
+        if "Position: SmallBlind" in line:
+            curr_player = 2
+        if "Position: BigBlind" in line:
+            curr_player = 3
+
+        split_line = line.split("|")
+        if len(split_line) == 16:
+            if split_line[0][0] == " ":
+                continue
+            else:
+                hand_idx = hand_str_2_idx(hand=split_line[0])
+
+                for idx, entry_values in enumerate(split_line[1:-1]):
+                    entry_values_split = entry_values.split(" ")
+                    sign = entry_values_split[1]
+                    diff = float(entry_values_split[2])
+                    if sign == "A":
+                        sign = 1
+                    elif sign == "F":
+                        sign = -1
                     else:
-                        end = "\t\t"
-                    print(val, end=end)
-                print(end="\n")
+                        raise ValueError("Invalid sign")
+                    data_results[curr_player, curr_t, idx, hand_idx] = sign * diff
 
 
-directory = "./"
-suffix = "*.log"
+    return data_results
 
 
-if __name__ == '__main__':
-    res_dict = create_res_dict()
-    for log_idx, log in enumerate(glob.glob(os.path.join(directory, suffix))):
-        co_table, de_table, sb_table, bb_table = extract_tables(logger=log)
 
-        parse_table(table=co_table, indices=[1], states=["CO"], res_dict=res_dict)
-        parse_table(table=de_table, indices=[2, 3], states=["DE", "DE_CO"], res_dict=res_dict)
-        parse_table(table=sb_table, indices=[4, 5, 6, 7], states=["SB", "SB_CO", "SB_DE", "SB_CO_DE"], res_dict=res_dict)
-        parse_table(table=bb_table, indices=[8, 9, 10, 11, 12, 13, 14], states=["BB_CO", "BB_DE", "BB_SB", "BB_CO_DE", "BB_CO_SB", "BB_DE_SB", "BB_CO_DE_SB"], res_dict=res_dict)
 
-        print_dict(res_dict=res_dict, n=log_idx+1)
-        print("\n")
+def mwu_data():
+    f_data_results = []
+    for filename in os.listdir("./"):
+        if filename.startswith("MWU"):
+            data_res = parse_mwu_log(filename)
+            f_data_results.append(data_res)
+
+    min_t = min([data_res.shape[1] for data_res in f_data_results])
+    for i, data_res in enumerate(f_data_results):
+        f_data_results[i] = data_res[:, :min_t, :, :]
+    f_data_results = np.array(f_data_results)
+    return f_data_results
+
+
+def bandit_data():
+    f_data_results = []
+    for filename in os.listdir("./"):
+        if filename.startswith("bandit"):
+            data_res = parse_bandit_log(filename)
+            f_data_results.append(data_res)
+
+    min_t = min([data_res.shape[1] for data_res in f_data_results])
+    for i, data_res in enumerate(f_data_results):
+        f_data_results[i] = data_res[:, :min_t, :, :]
+    f_data_results = np.array(f_data_results)
+    return f_data_results
+
+
+data_results = bandit_data()
+mean_data_results = np.mean(data_results, axis=0)
+std_data_results = np.std(data_results, axis=0)
+
+for hand in ['72o', '62o', '54o']:
+    t = range(data_results.shape[2])
+    m = mean_data_results[player_d["Dealer"], :, situation_d["DE"], hand_str_2_idx(hand)]
+    s = std_data_results[player_d["Dealer"], :, situation_d["DE"], hand_str_2_idx(hand)]
+    plt.plot(t, m, label=hand)
+    plt.fill_between(t, m-s, m+s, color='gray', alpha=0.2)
+
+plt.title("Data")
+plt.legend()
+plt.grid()
+plt.xlabel("#Iteration * 10**7")
+plt.ylabel("<P>")
+plt.show()
+
+
+
+
+
+0/0
+
+
+data_results = mwu_data()
+
+mean_data_results = np.mean(data_results, axis=0)
+std_data_results = np.std(data_results, axis=0)
+
+for hand in ['K5s']:
+    t = range(data_results.shape[2])
+    m = mean_data_results[player_d["Dealer"], :, situation_d["DE"], hand_str_2_idx(hand)]
+    s = std_data_results[player_d["Dealer"], :, situation_d["DE"], hand_str_2_idx(hand)]
+    plt.plot(t, m, label=hand)
+    plt.fill_between(t, m-s, m+s, color='gray', alpha=0.2)
+
+plt.title("Data")
+plt.legend()
+plt.grid()
+plt.xlabel("#Iteration * 10**7")
+plt.ylabel("<P>")
+plt.show()
+
+
 
